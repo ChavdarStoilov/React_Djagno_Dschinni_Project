94be6d0ae1f6c02a24fa83124306ab22b4b46263
Chavdar Stoilov <stoilov.chavdar@gmail.com>
2023-11-10 14:35:51 +0200

    Fix prodcut page

--- front-end/dschinni/src/components/Products.jsx
+++ front-end/dschinni/src/components/Products.jsx
@@ -8,10 +8,6 @@ export default function Products() {
                     <div className="row">
                         <div className="col-sm-12">
                             <h1 className="product_taital">Our Products</h1>
-                            <p className="product_text">
-                                incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation
-                            </p>
                         </div>
                     </div>
                     <div className="product_section_2 layout_padding">
@@ -128,122 +124,6 @@ export default function Products() {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="product_box">
-                                    <h4 className="bursh_text">Beauty Bursh</h4>
-                                    <p className="lorem_text">
-                                        incididunt ut labore et dolore magna
-                                        aliqua. Ut enim{" "}
-                                    </p>
-                                    <img
-                                        src="images/img-5.png"
-                                        className="image_1"
-                                    />
-                                    <div className="btn_main">
-                                        <div className="buy_bt">
-                                            <ul>
-                                                <li className="active">
-                                                    <a href="#">Buy Now</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#">Buy Now</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <h3 className="price_text">
-                                            Price $30
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="product_box">
-                                    <h4 className="bursh_text">Beauty Bursh</h4>
-                                    <p className="lorem_text">
-                                        incididunt ut labore et dolore magna
-                                        aliqua. Ut enim{" "}
-                                    </p>
-                                    <img
-                                        src="images/img-6.png"
-                                        className="image_1"
-                                    />
-                                    <div className="btn_main">
-                                        <div className="buy_bt">
-                                            <ul>
-                                                <li className="active">
-                                                    <a href="#">Buy Now</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#">Buy Now</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <h3 className="price_text">
-                                            Price $30
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="product_box">
-                                    <h4 className="bursh_text">Beauty Bursh</h4>
-                                    <p className="lorem_text">
-                                        incididunt ut labore et dolore magna
-                                        aliqua. Ut enim{" "}
-                                    </p>
-                                    <img
-                                        src="images/img-7.png"
-                                        className="image_1"
-                                    />
-                                    <div className="btn_main">
-                                        <div className="buy_bt">
-                                            <ul>
-                                                <li className="active">
-                                                    <a href="#">Buy Now</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#">Buy Now</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <h3 className="price_text">
-                                            Price $30
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="product_box">
-                                    <h4 className="bursh_text">Beauty Bursh</h4>
-                                    <p className="lorem_text">
-                                        incididunt ut labore et dolore magna
-                                        aliqua. Ut enim{" "}
-                                    </p>
-                                    <img
-                                        src="images/img-8.png"
-                                        className="image_1"
-                                    />
-                                    <div className="btn_main">
-                                        <div className="buy_bt">
-                                            <ul>
-                                                <li className="active">
-                                                    <a href="#">Buy Now</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#">Buy Now</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <h3 className="price_text">
-                                            Price $30
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
                             <div className="col-lg-3 col-sm-6">
                                 <div className="product_box">
                                     <h4 className="bursh_text">Beauty Bursh</h4>
