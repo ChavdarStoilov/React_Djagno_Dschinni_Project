d942d8c0096572e1251c0738de2ed39093b4dba8
Chavdar Stoilov <80538498+ChavdarStoilov@users.noreply.github.com>
2023-11-21 10:35:20 +0200

    Add missing file from front end

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -6,9 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 3000, // This is the port which we will use in docker
-    // Thanks @sergiomoura for the window fix
-    // add the next lines if you're using windows and hot reload doesn't work
+    port: 3000, 
      watch: {
        usePolling: true
      }
